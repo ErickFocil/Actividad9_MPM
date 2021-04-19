bd9b06f7952db9b5bea4441ceffec2077b904383
user185916 <user185916@74-80-250-245.ic2net.net>
2021-04-19 07:58:25 -0700

    Actividad arreglada

---
+++ Prototipo2/Prototipo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototipo2
+//
+//  Created by user185916 on 4/19/21.
+//  Copyright © 2021 user185916. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
